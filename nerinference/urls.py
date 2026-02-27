f54139992dd710dee655e38352b1fa18fd2d961e
virajyadav <virajyadav2497@gmail.com>
2026-02-28 00:19:29 +0530

    feat(api): expose mask endpoint in routing

--- nerinference/urls.py
+++ nerinference/urls.py
@@ -3,11 +3,15 @@ from .views import PredictView
 from .views import RegisterView
 from .views import LoginView 
 from .views import HealthCheckView
+from .views import ApiRootView
+from .views import MaskPIIView
 
 
 urlpatterns = [
+    path("", ApiRootView.as_view(), name="api-root"),
     path("register/", RegisterView.as_view(), name="register"),
     path("login/", LoginView.as_view(), name="login"),
     path("predict/", PredictView.as_view(), name="predict"),
+    path("mask/", MaskPIIView.as_view(), name="mask"),
     path("health/",HealthCheckView.as_view(),name="health")
 ]
