22222fe8cc903023de2eef15961cebe6e94b4cfb
virajyadav <virajyadav2497@gmail.com>
2026-02-28 00:19:18 +0530

    chore(config): disable default DRF authentication for local testing

--- nermlops/settings.py
+++ nermlops/settings.py
@@ -48,11 +48,8 @@ INSTALLED_APPS = [
 ]
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.TokenAuthentication',
-    ],
     'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
+        'rest_framework.permissions.AllowAny',
     ],
 }
 
